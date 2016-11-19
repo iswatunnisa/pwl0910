42b93ad969553f89abc28836c5b6b3839e25e5b7
iswatunnisa <iswatunnisa.is@students.amikom.ac.id>
2016-11-19 14:59:41 +0700

    version 1.1

--- nilai.php
+++ nilai.php
@@ -4,7 +4,7 @@ if ($nilai>100||$nilai<0) {
 	echo "Nilai yang anda masukkan tidak valid";
 }
 elseif ($nilai>80) {
-	echo "Nilai Anda adalah A";
+	echo "Nilai Anda adalah A, Sangat Bagus";
 }
 elseif ($nilai>60) {
 	echo "Nilai Anda adalah B";
@@ -16,6 +16,7 @@ elseif ($nilai>20) {
 	echo "Nilai Anda adalah D";
 }
 else {
-	echo "Nilai anda adalah E";
+	echo "Nilai anda adalah E, Tolong Belajar Lagi";
 }
+
 ?>
